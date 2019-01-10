f48fb779cc7e55ec5ddf761d09b562f51034c00a
Vladimir Fominykh <stoyakovi4@gmail.com>
2019-01-10 13:37:37 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//1
+var budget = prompt("Ваш бюджет?");
+var nameShop = prompt("Название вашего магазина?");
+//2
+// console.log(budget);
+// console.log(nameShop);
+//3
+mainList = {
+	budget: 1,
+	nameShop: "JS",
+	shopGoods: [1,2,3],
+	open: true
+}
+shopGoods[0] = prompt("Какой тип товара будем продавать?");
+shopGoods[1] = prompt("Какой тип товара будем продавать?");
+shopGoods[2] = prompt("Какой тип товара будем продавать?");
+console.log(shopGoods);
+
+
+
+
+
+
+
+
+
+
